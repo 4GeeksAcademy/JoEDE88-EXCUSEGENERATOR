e346f14365bd2a6915f40a6a0ff7397805a1be2b
Joe <joediaz88@hotmail.com>
2024-11-04 20:23:12 +0000

    creada función para generar excusas random

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+
+  let what = ["my homework ", "my phone ", "the car "];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomPhrase =
+    who[Math.floor(Math.random() * 4)] +
+    action[Math.floor(Math.random() * 4)] +
+    what[Math.floor(Math.random() * 4)] +
+    when[Math.floor(Math.random() * 4)];
+
+  document.getElementById("excusas").innerHTML = randomPhrase;
 };
